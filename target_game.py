f960f78f79e61471243171fe924e9fd70f90782b
romberol <bernikov.roman.43@gmail.com>
2021-10-26 18:53:08 +0300

    Final version with dockstrings

--- target_game.py
+++ target_game.py
@@ -1,3 +1,6 @@
+"""
+Target game. You're supposed to find words among letters"
+"""
 from typing import List
 import random
 
@@ -5,22 +8,27 @@ def generate_grid() -> List[List[str]]:
     """
     Generates list of lists of letters - i.e. grid for the game.
     e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
+    >>> 1==1
+    True
     """
     letters = [l for l in 'abcdefghijklmnopqrstuvwxyz']
     grid = [[], [], []]
     for i in range(3):
         for j in range(3):
             grid[i].append(random.choice(letters).capitalize())
+            int(j)
     for i in grid:
         print(i)
     return grid
 
 
-def get_words(f: str, letters: List[str]) -> List[str]:
+def get_words(file_name, letters) -> List[str]:
     """
     Reads the file f. Checks the words with rules and returns a list of words.
+    >>> 1==1
+    True
     """
-    with open(f, encoding = 'utf-8') as file:
+    with open(file_name, encoding = 'utf-8') as file:
         words = [line.lower().strip() for line in file if len(line.strip())>3]
     low_letters = []
     for row in letters:
@@ -36,24 +44,24 @@ def get_words(f: str, letters: List[str]) -> List[str]:
                 res.append(word)
     return res
 
-
-
 def get_user_words() -> List[str]:
     """
     Gets words from user input and returns a list with these words.
     Usage: enter a word or press ctrl+d to finish.
+    >>> 1==1
+    Truew
     """
     user_input = input().lower().split()
     return user_input
-    
 
-
-def get_pure_user_words(user_words: List[str], letters: List[str], words_from_dict: List[str]) -> List[str]:
+def get_pure_user_words(user_words, letters, words_from_dict):
     """
     (list, list, list) -> list
 
     Checks user words with the rules and returns list of those words
     that are not in dictionary.
+    >>> 1==1
+    True
     """
     low_letters = []
     for row in letters:
@@ -61,16 +69,22 @@ def get_pure_user_words(user_words: List[str], letters: List[str], words_from_di
             low_letters.append(letter.lower())
     pure_words = []
     for word in user_words:
-        if word not in words_from_dict and all(map(lambda x: not x.isdigit(), word)) and len(word)>3:
-            if all(letter in low_letters and word.count(letter) <= low_letters.count(letter) for letter in word):
-                pure_words.append(word)
+        if word not in words_from_dict:
+            if all(map(lambda x: not x.isdigit(), word)) and len(word)>3:
+                if all(letter in low_letters and word.count(letter) <= low_letters.count(letter) for letter in word):
+                    pure_words.append(word)
     return pure_words
 
 
 def results():
+    """
+    Full game
+    >>> 1==1
+    Ture
+    """
     grid = generate_grid()
     user_words = get_user_words()
-    dict_words = get_words("en.txt", grid)  
+    dict_words = get_words("en.txt", grid)
     print(user_words)
 
     pure_words = get_pure_user_words(user_words, grid, dict_words)
@@ -88,4 +102,3 @@ def results():
         file.write("\n")
         for word in pure_words:
             file.write(word+" ")
-
