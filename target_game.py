57252ebac20fd3ae50af3752f7c74928f12b6175
romberol <bernikov.roman.43@gmail.com>
2021-10-26 14:13:08 +0300

    File added with functions get_words() and generate_grid()

--- target_game.py
+++ target_game.py
@@ -1,18 +1,38 @@
 from typing import List
+import random
 
 def generate_grid() -> List[List[str]]:
     """
     Generates list of lists of letters - i.e. grid for the game.
     e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
     """
-    pass
+    letters = [l for l in 'abcdefghijklmnopqrstuvwxyz']
+    grid = [[], [], []]
+    for i in range(3):
+        for j in range(3):
+            grid[i].append(random.choice(letters).capitalize())
+    return grid
 
 
 def get_words(f: str, letters: List[str]) -> List[str]:
     """
     Reads the file f. Checks the words with rules and returns a list of words.
     """
-    pass
+    with open(f, encoding = 'utf-8') as file:
+        words = [line.strip() for line in file if len(line.strip())>3]
+    low_letters = []
+    for row in letters:
+        for letter in row:
+            low_letters.append(letter.lower())
+    res = []
+    for word in words:
+        for letter in word:
+            if letter not in low_letters or word.count(letter) > low_letters.count(letter):
+                break
+        else:
+            if low_letters[4] in word:
+                res.append(word)
+    print(res, low_letters)
 
 
 
@@ -21,7 +41,7 @@ def get_user_words() -> List[str]:
     Gets words from user input and returns a list with these words.
     Usage: enter a word or press ctrl+d to finish.
     """
-    pass
+    
 
 
 def get_pure_user_words(user_words: List[str], letters: List[str], words_from_dict: List[str]) -> List[str]:
@@ -36,3 +56,6 @@ def get_pure_user_words(user_words: List[str], letters: List[str], words_from_di
 
 def results():
     pass
+
+get_words("E:/UCU/OP/ЛР 6/en.txt", generate_grid())
+#print(get_user_words())
